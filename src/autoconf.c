braice/MuMuDVB

commit 8781093d840fca98827157a2302bcf6ad87899ca

Autoconf : we don't update the channel when the version was not really changed

src/mumudvb.c
@@ -1753,19 +1753,25 @@ main (int argc, char **argv)
 
 			if(get_ts_packet(temp_buffer_from_dvr,channels[curr_channel].pmt_packet))
 			  {
-			    log_message(MSG_DEBUG,"Autoconfiguration : PMT packet updated, we have now to check if there is new things\n");
-			    /*We've got the FULL PMT packet*/
-			    if(autoconf_read_pmt(channels[curr_channel].pmt_packet, &channels[curr_channel], tuneparams.card, asked_pid, number_chan_asked_pid, &fds)==0)
-			    {
-			      //plop
-			      /**@todo cam update*/
-			      if(channels[curr_channel].need_cam_ask==CAM_ASKED)
-				channels[curr_channel].need_cam_ask=CAM_NEED_ASK;
-			      update_pmt_version(&channels[curr_channel]);
-			      channels[curr_channel].pmt_needs_update=0;
-			    }
+			    if(pmt_need_update(&channels[curr_channel],channels[curr_channel].pmt_packet->packet))
+			      {
+				log_message(MSG_DEBUG,"Autoconfiguration : PMT packet updated, we have now to check if there is new things\n");
+				/*We've got the FULL PMT packet*/
+				if(autoconf_read_pmt(channels[curr_channel].pmt_packet, &channels[curr_channel], tuneparams.card, asked_pid, number_chan_asked_pid, &fds)==0)
+				  {
+				    if(channels[curr_channel].need_cam_ask==CAM_ASKED)
+				      channels[curr_channel].need_cam_ask=CAM_NEED_ASK;
+				    update_pmt_version(&channels[curr_channel]);
+				    channels[curr_channel].pmt_needs_update=0;
+				  }
+				else
+				  channels[curr_channel].pmt_packet->empty=1;
+			      }
 			    else
-			      channels[curr_channel].pmt_packet->empty=1;
+			      {
+				log_message(MSG_DEBUG,"Autoconfiguration : False alert, nothing to do\n");
+				channels[curr_channel].pmt_needs_update=0;
+			      }
 			  }
 		    }
 		}

src/ts.h
@@ -449,15 +449,6 @@ typedef struct {
   int len;
   /**the buffer*/
   unsigned char packet[4096];
-#ifndef LIBDVBEN50221 //useful only if we don't use the libdvben50221 from dvb_apps 
-  /**@todo remove*/
-  /** do we need to descramble ? for vlc cam support*/
-  int need_descr;
-  /** program number for vlc cam support*/
-  int i_program_number; 
-  /**the buffer for the cam (for cam_support)*/
-  unsigned char converted_packet[4096]; 
-#endif
 }mumudvb_ts_packet_t;
 
 

commit 7fa43e3983aeefa238b14a11f6d9ca41b9825ef5

Autoconf : fixed a regression introduced by 8781093d840fca98827157a2302bcf6ad87899ca

src/autoconf.c
@@ -1349,15 +1349,23 @@ int autoconf_parse_vct_channel(unsigned char *buf, autoconf_parameters_t *parame
  *@param channel the channel for which we have to check
  *@param buf : the received buffer
  */
-int pmt_need_update(mumudvb_channel_t *channel, unsigned char *buf)
+int pmt_need_update(mumudvb_channel_t *channel, unsigned char *buf,int ts_header)
 {
-  pmt_t       *pmt=(pmt_t*)(buf+TS_HEADER_LEN);
-  ts_header_t *header=(ts_header_t *)buf;
+  pmt_t       *pmt;
+  ts_header_t *header;
 
-  if(header->payload_unit_start_indicator) //It's the beginning of a new packet
+  if(ts_header)
+    {
+      pmt=(pmt_t*)(buf+TS_HEADER_LEN);
+      header=(ts_header_t *)buf;
+    }
+  else
+      pmt=(pmt_t*)(buf);
+
+  if(!ts_header || header->payload_unit_start_indicator) //It's a packet without header or the beginning of a new packet 
     if(pmt->version_number!=channel->pmt_version)
       {
-	log_message(MSG_DEBUG,"Autoconfiguration : PMT version changed, channel %s . stored version : %d, new: %d. The CRC have to be checked\n",channel->name,channel->pmt_version,pmt->version_number);
+	log_message(MSG_DEBUG,"Autoconfiguration : PMT version changed, channel %s . stored version : %d, new: %d.\n",channel->name,channel->pmt_version,pmt->version_number);
 	return 1;
       }
   return 0;

src/autoconf.h
@@ -109,7 +109,7 @@ int services_to_channels(autoconf_parameters_t parameters, mumudvb_channel_t *ch
 int autoconf_finish_full(int *number_of_channels, mumudvb_channel_t *channels, autoconf_parameters_t *autoconf_vars, int common_port, int card, fds_t *fds,uint8_t *asked_pid, uint8_t *number_chan_asked_pid, int multicast_ttl);
 void autoconf_end(int card, int number_of_channels, mumudvb_channel_t *channels, uint8_t *asked_pid, uint8_t *number_chan_asked_pid, fds_t *fds);
 void autoconf_free_services(mumudvb_service_t *services);
-int pmt_need_update(mumudvb_channel_t *channel, unsigned char *buf);
+int pmt_need_update(mumudvb_channel_t *channel, unsigned char *buf, int ts_header);
 void update_pmt_version(mumudvb_channel_t *channel);
 
 

src/mumudvb.c
@@ -1734,7 +1734,7 @@ main (int argc, char **argv)
 		  if(!channels[curr_channel].pmt_needs_update)
 		    {
 		      //Checking without crc32, it there is a change we get the full packet for crc32 checking
-		      channels[curr_channel].pmt_needs_update=pmt_need_update(&channels[curr_channel],temp_buffer_from_dvr);
+		      channels[curr_channel].pmt_needs_update=pmt_need_update(&channels[curr_channel],temp_buffer_from_dvr,1);
 		      
 		      if(channels[curr_channel].pmt_needs_update&&channels[curr_channel].pmt_packet) //It needs update we mark the packet as empty
 			channels[curr_channel].pmt_packet->empty=1;
@@ -1755,9 +1755,9 @@ main (int argc, char **argv)
 
 			if(get_ts_packet(temp_buffer_from_dvr,channels[curr_channel].pmt_packet))
 			  {
-			    if(pmt_need_update(&channels[curr_channel],channels[curr_channel].pmt_packet->packet))
+			    if(pmt_need_update(&channels[curr_channel],channels[curr_channel].pmt_packet->packet,0))
 			      {
-				log_message(MSG_DEBUG,"Autoconfiguration : PMT packet updated, we have now to check if there is new things\n");
+				log_message(MSG_DETAIL,"Autoconfiguration : PMT packet updated, we have now to check if there is new things\n");
 				/*We've got the FULL PMT packet*/
 				if(autoconf_read_pmt(channels[curr_channel].pmt_packet, &channels[curr_channel], tuneparams.card, asked_pid, number_chan_asked_pid, &fds)==0)
 				  {