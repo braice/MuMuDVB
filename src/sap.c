cd3bf332d26e6853658ae836ca90a83287f9e53c
Brice DUBOST <mumudvb@braice.net>
2010-10-03 16:07:29 +0200

    Typos

--- src/sap.c
+++ src/sap.c
@@ -1,22 +1,22 @@
-/* 
- * mumudvb - UDP-ize a DVB transport stream.
- * 
- * (C) 2008-2009 Brice DUBOST
- * 
+/*
+ * MuMuDVB - Stream a DVB transport stream.
+ *
+ * (C) 2008-2010 Brice DUBOST
+ *
  * The latest version can be found at http://mumudvb.braice.net
- * 
+ *
  * Copyright notice:
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -25,7 +25,7 @@
 /** @file
  * @brief File for Session Announcement Protocol Announces
  * @author Brice DUBOST
- * @date 2008-2009
+ * @date 2008-2010
  */
 
 #include "sap.h"
@@ -141,7 +141,6 @@ int read_sap_configuration(sap_parameters_t *sap_vars, mumudvb_channel_t *curren
 
   return 1;//We found something for sap, we tell main to go for the next line
 
-  
 }
 
 
@@ -184,7 +183,7 @@ void sap_send(sap_parameters_t *sap_vars, int num_messages)
   int sent_messages=0;
   mumudvb_sap_message_t *sap_messages;
   sap_messages=sap_vars->sap_messages;
-  
+
   for( curr_message=0; curr_message<num_messages;curr_message++)
     {
       if(sap_messages[curr_message].to_be_sent)
@@ -343,21 +342,21 @@ int sap_add_program(mumudvb_channel_t *channel, sap_parameters_t *sap_vars, mumu
 
 
   /**@subsection time session : tell when the session is active
-     
+
      t=...
-     
+
      permanent program : t=0 0
 
      @subsection attributes : group and co, we'll take the minisapserver ones
      a=...
 
      a=tool:mumudvb-VERSION
-     
+
      a=type:broadcast
 
      a=x-plgroup: //channel's group
   */
-  //plopplop
+
   sprintf(temp_string,"t=0 0\r\na=tool:mumudvb-%s\r\na=type:broadcast\r\n", VERSION);
   if( (sap_message->len+payload_len+strlen(temp_string))>1024)
     {
