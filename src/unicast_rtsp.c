310fa0b109bb4bea8b0ae1c743fe7f7028d9443c
Ludovic BOUÉ <ludovic.boue@gmail.com>
2011-05-25 00:08:58 +0200

    Fix RTSP TEARDOWN reply
    Removing the client from the channel when TEARDOWN was receive was not working

--- src/unicast_rtsp.c
+++ src/unicast_rtsp.c
@@ -59,6 +59,7 @@ extern int Interrupted;
 
 //from unicast_client.c
 int channel_add_unicast_client(unicast_client_t *client,mumudvb_channel_t *channel);
+int unicast_del_client(unicast_parameters_t *unicast_vars, unicast_client_t *client, mumudvb_channel_t *channels);
 
 
 /** @todo handle non keepalive clients, and auto disconnect to old clients without streaming */
@@ -68,7 +69,7 @@ int unicast_send_rtsp_options (int Socket, int CSeq);
 int unicast_send_rtsp_setup (unicast_client_t *client, int CSeq, int Tsprt_type);
 int unicast_send_rtsp_play (int Socket, int CSeq, unicast_client_t *client, mumudvb_channel_t *channels, int number_of_channels);
 int unicast_send_rtsp_setup (unicast_client_t *client, int CSeq, int Tsprt_type);
-int unicast_send_rtsp_teardown (int Socket, int CSeq, unicast_client_t *client , mumudvb_channel_t *channels, unicast_parameters_t *unicast_vars, fds_t *fds);
+int unicast_send_rtsp_teardown (int Socket, int CSeq, unicast_client_t *client , mumudvb_channel_t *channels, unicast_parameters_t *unicast_vars, fds_t *fds, int number_of_channels);
 
 /** @brief Deal with an incoming message on the unicast client connection
 * This function will store and answer the RTSP requests
@@ -123,13 +124,13 @@ int unicast_handle_rtsp_message(unicast_parameters_t *unicast_vars, unicast_clie
 
   if(strstr(client->buffer,"DESCRIBE")==client->buffer)
   {
-    unicast_send_rtsp_describe(client->Socket, CSeq);
+    unicast_send_rtsp_describe(client->Socket, CSeq);  // send response trough RTSP Server socket
     unicast_flush_client(client);
     return 0;
   }
   else if(strstr(client->buffer,"OPTIONS")==client->buffer)
   {
-    unicast_send_rtsp_options(client->Socket, CSeq);
+    unicast_send_rtsp_options(client->Socket, CSeq);  // send response trough RTSP Server socket
     unicast_flush_client(client);
     return 0;
   }
@@ -223,34 +224,42 @@ int unicast_handle_rtsp_message(unicast_parameters_t *unicast_vars, unicast_clie
     }
     //client->rtsp_server_port=ntohs(client->rtsp_SocketAddr.sin_port);
     client->rtsp_server_port=4242;
-    unicast_send_rtsp_setup(client, CSeq, TransportType);
+    unicast_send_rtsp_setup(client, CSeq, TransportType);  // send response trough RTSP Server socket
     unicast_flush_client(client);
     return 0;
   }
   else if(strstr(client->buffer,"PLAY")==client->buffer)
   {
-    iRet=unicast_send_rtsp_play(client->Socket, CSeq, client, channels, number_of_channels);
+    iRet=unicast_send_rtsp_play(client->Socket, CSeq, client, channels, number_of_channels);  // send response trough RTSP Server socket
     if(iRet)
       return CLOSE_CONNECTION;
     unicast_flush_client(client);
+
+/** 
+    log_message(MSG_DETAIL,"Unicast : We delete the client %s:%d, socket %d\n",inet_ntoa(client->Socket.sin_addr), ntohs(client->Socket.sin_port), client->Socket);
     if (client->Socket >= 0)
     {
       log_message(MSG_DEBUG,"Unicast: RTSP We close the control connection\n");
       unicast_close_connection(unicast_vars, fds, client->Socket, channels, 0);
       client->Socket=0;
       client->Control_socket_closed=1;
-      /** @todo important : test if the client already exist when a new client arrives, delete dead clients*/
-    }
+
+
+      
+    }*/
+
+/** @todo important : test if the client already exist when a new client arrives, delete dead clients*/
+
     return 0;
   }
   else if(strstr(client->buffer,"TEARDOWN")==client->buffer)
   {
-    unicast_send_rtsp_teardown(client->Socket, CSeq, client, channels, unicast_vars, fds);
+    unicast_send_rtsp_teardown(client->Socket, CSeq, client, channels, unicast_vars, fds, number_of_channels);
     return CLOSE_CONNECTION;
   }
   else
   {
-    log_message(MSG_WARN,"rate ... \n");
+    log_message(MSG_WARN,"La commande n'est pas comprise pas le serveur\n");
     unicast_flush_client(client);
   }
 
@@ -389,7 +398,7 @@ int unicast_send_rtsp_options (int Socket, int CSeq)
  */
 int unicast_send_rtsp_setup (unicast_client_t *client, int CSeq, int Tsprt_type)
 {
-  int Socket=client->Socket;
+  int Socket=client->Socket;	// On ne recupere pas la socket en parametre mais depuis la structure "client"
   char TransportType[20], broadcast_type[20];
   unicast_reply_t* reply = unicast_reply_init();
   if (NULL == reply)
@@ -485,7 +494,7 @@ int unicast_send_rtsp_play (int Socket, int CSeq, unicast_client_t *client, mumu
 
   if (0 != unicast_reply_free(reply))
   {
-    log_message(MSG_INFO,"Unicast : Error when releasing the HTTP reply after sendinf it\n");
+    log_message(MSG_INFO,"Unicast : Error when releasing the RTSP reply after sendinf it\n");
     return -1;
   }
 
@@ -496,8 +505,14 @@ int unicast_send_rtsp_play (int Socket, int CSeq, unicast_client_t *client, mumu
  *
  * @param Socket the socket on wich the information have to be sent
  */
-int unicast_send_rtsp_teardown (int Socket, int CSeq, unicast_client_t *client , mumudvb_channel_t *channels, unicast_parameters_t *unicast_vars, fds_t *fds)
+int unicast_send_rtsp_teardown (int Socket, int CSeq, unicast_client_t *client , mumudvb_channel_t *channels, unicast_parameters_t *unicast_vars, fds_t *fds, int number_of_channels)
 {
+  int err404=0;
+  int requested_channel;
+  requested_channel=0;
+  char *tempstring;
+  err404=0;
+
   unicast_reply_t* reply = unicast_reply_init();
   if (NULL == reply)
   {
@@ -507,8 +522,43 @@ int unicast_send_rtsp_teardown (int Socket, int CSeq, unicast_client_t *client ,
 
   log_message(MSG_INFO,"Requete TEARDOWN RTSP\n");
 
+  tempstring=strstr(client->buffer,"rtsp://");
+  tempstring=strstr(tempstring+7,"/");
+  if(tempstring)
+    requested_channel=parse_channel_path(tempstring,&err404, number_of_channels, channels);
+  else
+  {
+    log_message(MSG_INFO,"Unicast : Malformed TEARDOWN request, we return 404\n");
+    err404=1;
+  }
+  if(!requested_channel)
+    err404=1;
+  if(err404)
+  {
+    log_message(MSG_INFO,"Unicast : Path not found i.e. 404\n");
+    unicast_reply_write(reply, "Session: %s\r\n", client->session);
+    rtsp_reply_prepare_headers(reply, 404, CSeq);
+    rtsp_reply_send(reply, Socket, "text/plain");;
+    if (0 != unicast_reply_free(reply)) {
+      log_message(MSG_INFO,"Unicast : Error when releasing the RTSP reply after sendinf it\n");
+    }
+    return CLOSE_CONNECTION; //to delete the client
+  }
+
+  unicast_reply_write(reply, "Session: %s\r\n", client->session);
   rtsp_reply_prepare_headers(reply, 200, CSeq);
   rtsp_reply_send(reply, Socket, "text/plain");
+
+  if(requested_channel)
+  {
+     log_message(MSG_INFO,"Unicast : TEARDOWN channel %d\n",requested_channel);
+    // TODO Supprimer l'IP du client dans la liste des IP unicast.
+    //client->channel=requested_channel-1;
+    log_message(MSG_INFO,"Unicast : On supprime la chaine %d de la liste de diffusion de la socket\n", requested_channel-1);
+
+    log_message(MSG_INFO,"Unicast : Valeur dans le tableau channels de la socket %d\n", &channels[requested_channel-1]);
+    //unicast_del_client(unicast_vars, client, &channels[requested_channel-1]);
+  }
 
   if (0 != unicast_reply_free(reply))
   {
