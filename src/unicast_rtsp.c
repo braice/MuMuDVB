43c1328e6a31b4f7b856391d7922726900c36c64
Ludovic BOUÉ <ludovic.boue@gmail.com>
2011-06-02 18:39:47 +0200

    Prepare new error types

--- src/unicast_rtsp.c
+++ src/unicast_rtsp.c
@@ -74,7 +74,6 @@ int unicast_send_rtsp_teardown (int Socket, int CSeq, unicast_client_t *client ,
 /** @brief Deal with an incoming message on the unicast client connection
 * This function will store and answer the RTSP requests
 *
-*
 * @param unicast_vars the unicast parameters
 * @param client The client from which the message was received
 * @param channels the channel array
@@ -93,16 +92,15 @@ int unicast_handle_rtsp_message(unicast_parameters_t *unicast_vars, unicast_clie
     return iRet;
   }
 
-  //We search for the end of the HTTP request
+  //We search for the end of the RTSP request
   if(!(strlen(client->buffer)>5 && strstr(client->buffer, "\n\r\n\0")))
     return 0;
 
-
   log_message(MSG_DEBUG,"Unicast : End of RTSP request, we parse it\n");
-  // We implement RTSP protocol
+
   /* The client is initially supplied with an RTSP URL, on the form:
-      rtsp://server.address:port/object.sdp
-  */
+      rtsp://server.address:port/object.sdp */
+ 
   /** @todo important : test if the client already exist using the session number*/
   char *poscseq;
   int CSeq=0;
@@ -112,11 +110,11 @@ int unicast_handle_rtsp_message(unicast_parameters_t *unicast_vars, unicast_clie
   log_message(MSG_FLOOD,"Buffer : %s\n",client->buffer);
   if(poscseq==NULL)
   {
-    log_message(MSG_DEBUG," !!!!!!!!!!!!!!!! CSeq not found\n");
+    log_message(MSG_DEBUG,"Sequence number not found\n");
     /** @todo Implement an error following the RTSP protocol*/
     return CLOSE_CONNECTION; //tu n'est pas content ...
   }
-  log_message(MSG_DEBUG,"CSeq a la position %d\n", (int)(poscseq-(client->buffer)));
+  log_message(MSG_DEBUG,"CSeq found at position %d\n", (int)(poscseq-(client->buffer)));
   pos=(int)(poscseq-(client->buffer));
   pos+=strlen("Cseq: ");
   CSeq = atoi(client->buffer+pos); //replace by CSeq = atoi(poscseq+strlen("Cseq: "));
@@ -150,20 +148,17 @@ int unicast_handle_rtsp_message(unicast_parameters_t *unicast_vars, unicast_clie
     session[15]='\0';
     strcpy(client->session, session);
     log_message(MSG_DEBUG,"Session : %s\n", session );
-    // Parse le type de transport
-    int TransportType=TRANSPORT_UNDEFINED; // RTP/AVP | RTP/AVP/UDP = 0 , RTP/AVP/TCP=1
-    // Transport: RTP/AVP
-    /* transport-protocol  =    "RTP"
-       profile             =    "AVP"
-       lower-transport     =    "TCP" | "UDP" */
-    // Parse le type de transport
+
+    // We parse transport type
+    int TransportType=TRANSPORT_UNDEFINED; // RTP/AVP=0 | RTP/AVP/UDP = 0 , RTP/AVP/TCP=1
     pos_search=strstr(client->buffer ,"Transport:");
     if(pos_search==NULL)
     {
-      log_message(MSG_DEBUG,"!!!!!!!!!!!!!!!!!!!!!!!!Type de transport non trouve\n");
-      return CLOSE_CONNECTION; //tu n'est pas content ...
+      log_message(MSG_DEBUG,"Transport type not found\n");
+      return CLOSE_CONNECTION;
     }
-    log_message(MSG_DEBUG,"Type de transport a la position %d\n", (int)(pos_search-(client->buffer)));
+
+    log_message(MSG_DEBUG,"Transport type at position %d\n", (int)(pos_search-(client->buffer)));
     pos=(int)(pos_search-(client->buffer));
     pos+=strlen("Transport: ");
     if (!strncmp (client->buffer+pos, "RTP/AVP/TCP", strlen("RTP/AVP/TCP"))) {
@@ -182,8 +177,17 @@ int unicast_handle_rtsp_message(unicast_parameters_t *unicast_vars, unicast_clie
     }
     else
     {
-      log_message(MSG_INFO,"!!!!!!!!!!!!!!!!!!!!!!!!!Transport unknown");
-      return CLOSE_CONNECTION; /** @todo implement a RTSP error*/
+      log_message(MSG_INFO,"Error 461 : Unsupported transport type");
+/*      rtsp_reply_prepare_headers(reply, 461, CSeq);
+      rtsp_reply_send(reply, Socket,"text/plain");
+
+      if (0 != unicast_reply_free(reply))
+      {
+         log_message(MSG_INFO,"Unicast : Error when releasing the RTSP reply after sendinf it\n");
+      return -1;
+      }
+*/
+      return CLOSE_CONNECTION;
     }
 
     // Parse le port client
@@ -191,9 +195,12 @@ int unicast_handle_rtsp_message(unicast_parameters_t *unicast_vars, unicast_clie
     log_message(MSG_FLOOD,"Buffer : %s\n",client->buffer);
     if(pos_search==NULL)
     {
-      log_message(MSG_DEBUG,"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Port client non trouve\n");
-      /** @todo implement RTSP error */
-      return CLOSE_CONNECTION; //tu n'est pas content ...
+      log_message(MSG_DEBUG,"Client ports not found\n");
+      /** @todo implement RTSP error
+      rtsp_reply_prepare_headers(reply, 451, CSeq);
+      rtsp_reply_send(reply, Socket, "text/plain");  */
+
+      return CLOSE_CONNECTION;
     }
     pos=(int)(pos_search-(client->buffer));
     pos+=strlen("client_port=");
@@ -235,19 +242,6 @@ int unicast_handle_rtsp_message(unicast_parameters_t *unicast_vars, unicast_clie
       return CLOSE_CONNECTION;
     unicast_flush_client(client);
 
-/** 
-    log_message(MSG_DETAIL,"Unicast : We delete the client %s:%d, socket %d\n",inet_ntoa(client->Socket.sin_addr), ntohs(client->Socket.sin_port), client->Socket);
-    if (client->Socket >= 0)
-    {
-      log_message(MSG_DEBUG,"Unicast: RTSP We close the control connection\n");
-      unicast_close_connection(unicast_vars, fds, client->Socket, channels, 0);
-      client->Socket=0;
-      client->Control_socket_closed=1;
-
-
-      
-    }*/
-
 /** @todo important : test if the client already exist when a new client arrives, delete dead clients*/
 
     return 0;
@@ -259,7 +253,8 @@ int unicast_handle_rtsp_message(unicast_parameters_t *unicast_vars, unicast_clie
   }
   else
   {
-    log_message(MSG_WARN,"La commande n'est pas comprise pas le serveur\n");
+    // @todo return a 400 error
+    log_message(MSG_WARN,"This is not understood by the server\n");
     unicast_flush_client(client);
   }
 
@@ -279,8 +274,17 @@ int rtsp_reply_prepare_headers(unicast_reply_t *reply, int code, int CSeq)
     case 200:
       unicast_reply_write(reply, "200 OK\r\n");
       break;
+    case 400:
+      unicast_reply_write(reply, "400 Bad Request\r\n");
+      break;
     case 404:
       unicast_reply_write(reply, "404 Not found\r\n");
+      break;
+    case 451:
+      unicast_reply_write(reply, "451 Invalid parameter\r\n");
+      break;
+    case 454:
+      unicast_reply_write(reply, "454 Session Not Found\r\n");
       break;
     case 503:
       unicast_reply_write(reply, "503 Too many clients\r\n");
@@ -297,6 +301,9 @@ int rtsp_reply_prepare_headers(unicast_reply_t *reply, int code, int CSeq)
 }
 
 
+/** @brief Send a RTSP message
+ *  The method add headers the to a reply message
+ */
 int rtsp_reply_send(unicast_reply_t *reply, int socket, const char* content_type)
 {
   //we add the header information
@@ -334,7 +341,7 @@ int unicast_send_rtsp_describe (int Socket, int CSeq)
     log_message(MSG_INFO,"Unicast : Error when creating the RTSP reply\n");
     return -1;
   }
-  log_message(MSG_INFO,"Requete DESCRIBE RTSP\n");
+  log_message(MSG_INFO,"RTSP DESCRIBE request\n");
 
   char *TransportType="RTP/AVP";
   //if (TransportType) TransportType="RTP/AVP/TCP"; // =0:UDP, =1:TCP
@@ -346,17 +353,15 @@ int unicast_send_rtsp_describe (int Socket, int CSeq)
   unicast_reply_write(reply, "s=unknown\r\n");
   unicast_reply_write(reply, "i=unknown\r\n");
   unicast_reply_write(reply, "c=IN IP4 0.0.0.0\r\n");
-  //unicast_reply_write(reply, "c=IN IP4 239.100.1.0/2\r\n"); Pour le multicast RTSP
   unicast_reply_write(reply, "t=0 0\r\n");
   unicast_reply_write(reply, "m=video 0 %s 33\r\n", TransportType);
-  //   unicast_reply_write(reply, "a=control:rtsp://192.168.15.151:4242/rtsp/stream?namespace=1&service=201&flavour=ld\r\n");
 
   rtsp_reply_prepare_headers(reply, 200, CSeq);
   rtsp_reply_send(reply, Socket,"application/sdp");
 
   if (0 != unicast_reply_free(reply))
   {
-    log_message(MSG_INFO,"Unicast : Error when releasing the HTTP reply after sendinf it\n");
+    log_message(MSG_INFO,"Unicast : Error when releasing the RTSP reply after sendinf it\n");
     return -1;
   }
 
@@ -377,14 +382,14 @@ int unicast_send_rtsp_options (int Socket, int CSeq)
     return -1;
   }
 
-  log_message(MSG_INFO,"Requete OPTIONS RTSP\n");  ;
+  log_message(MSG_INFO,"RTSP OPTIONS request\n");  ;
 
   rtsp_reply_prepare_headers(reply, 200, CSeq);
   rtsp_reply_send(reply, Socket,"text/plain");
 
   if (0 != unicast_reply_free(reply))
   {
-    log_message(MSG_INFO,"Unicast : Error when releasing the HTTP reply after sendinf it\n");
+    log_message(MSG_INFO,"Unicast : Error when releasing the RTSP reply after sendinf it\n");
     return -1;
   }
 
@@ -407,7 +412,7 @@ int unicast_send_rtsp_setup (unicast_client_t *client, int CSeq, int Tsprt_type)
     return -1;
   }
 
-  log_message(MSG_INFO,"Requete SETUP RTSP\n");
+  log_message(MSG_INFO,"RTSP SETUP request\n");
   //unicast_reply_write(reply, RTSP_SETUP_REPLY);
 
   rtsp_reply_prepare_headers(reply, 200, CSeq);
@@ -419,9 +424,9 @@ int unicast_send_rtsp_setup (unicast_client_t *client, int CSeq, int Tsprt_type)
   else
     strcpy(TransportType,"RTP/AVP");
 
-  //TODO Get the broadcast_type from the DESCRIBE request
-  //if (Tsprt_type==2) broadcast_type="multicast";
-  //  else broadcast_type="unicast";
+  /* @todo Get the broadcast_type from the DESCRIBE request
+  if (Tsprt_type==2) broadcast_type="multicast";
+    else broadcast_type="unicast"; */
   strcpy( broadcast_type,"unicast");
   log_message(MSG_DEBUG, "Transport: %s;%s;mode=play;destination=%s;client_port=%d-%d;server_port=%d\r\n", TransportType, broadcast_type, client->client_ip, client->rtsp_client_port, client->rtsp_client_port+1, client->rtsp_server_port);
   unicast_reply_write(reply, "Transport: %s;%s;mode=play;destination=%s;client_port=%d-%d;server_port=%d\r\n", TransportType, broadcast_type, client->client_ip, client->rtsp_client_port, client->rtsp_client_port+1, client->rtsp_server_port);
@@ -429,7 +434,7 @@ int unicast_send_rtsp_setup (unicast_client_t *client, int CSeq, int Tsprt_type)
 
   if (0 != unicast_reply_free(reply))
   {
-    log_message(MSG_INFO,"Unicast : Error when releasing the HTTP reply after sendinf it\n");
+    log_message(MSG_INFO,"Unicast : Error when releasing the RTSP reply after sendinf it\n");
     return -1;
   }
 
@@ -486,7 +491,7 @@ int unicast_send_rtsp_play (int Socket, int CSeq, unicast_client_t *client, mumu
   if(requested_channel)
   {
      log_message(MSG_INFO,"Unicast : PLAY channel %d\n",requested_channel);
-    // TODO Ajouter l'IP du client dans la liste des IP unicast.
+    // @toto Add client IP into the unicast list.
     client->channel=requested_channel-1;
     channel_add_unicast_client(client, &channels[requested_channel-1]);
   }
@@ -507,11 +512,10 @@ int unicast_send_rtsp_play (int Socket, int CSeq, unicast_client_t *client, mumu
  */
 int unicast_send_rtsp_teardown (int Socket, int CSeq, unicast_client_t *client , mumudvb_channel_t *channels, unicast_parameters_t *unicast_vars, fds_t *fds, int number_of_channels)
 {
-  int err404=0;
+  int err=0;
   int requested_channel;
   requested_channel=0;
   char *tempstring;
-  err404=0;
 
   unicast_reply_t* reply = unicast_reply_init();
   if (NULL == reply)
@@ -525,19 +529,18 @@ int unicast_send_rtsp_teardown (int Socket, int CSeq, unicast_client_t *client ,
   tempstring=strstr(client->buffer,"rtsp://");
   tempstring=strstr(tempstring+7,"/");
   if(tempstring)
-    requested_channel=parse_channel_path(tempstring,&err404, number_of_channels, channels);
+    requested_channel=parse_channel_path(tempstring,&err, number_of_channels, channels);
   else
   {
-    log_message(MSG_INFO,"Unicast : Malformed TEARDOWN request, we return 404\n");
-    err404=1;
+    log_message(MSG_INFO,"Unicast : Malformed TEARDOWN request, we return 400\n");
   }
   if(!requested_channel)
-    err404=1;
-  if(err404)
+    err=404;
+  if(err!=0)
   {
     log_message(MSG_INFO,"Unicast : Path not found i.e. 404\n");
     unicast_reply_write(reply, "Session: %s\r\n", client->session);
-    rtsp_reply_prepare_headers(reply, 404, CSeq);
+    rtsp_reply_prepare_headers(reply, err, CSeq);
     rtsp_reply_send(reply, Socket, "text/plain");;
     if (0 != unicast_reply_free(reply)) {
       log_message(MSG_INFO,"Unicast : Error when releasing the RTSP reply after sendinf it\n");
@@ -551,18 +554,16 @@ int unicast_send_rtsp_teardown (int Socket, int CSeq, unicast_client_t *client ,
 
   if(requested_channel)
   {
-     log_message(MSG_INFO,"Unicast : TEARDOWN channel %d\n",requested_channel);
-    // TODO Supprimer l'IP du client dans la liste des IP unicast.
-    //client->channel=requested_channel-1;
-    log_message(MSG_INFO,"Unicast : On supprime la chaine %d de la liste de diffusion de la socket\n", requested_channel-1);
+     log_message(MSG_INFO,"Unicast : TEARDOWN for channel n°%d\n",requested_channel);
+    /* @todo Delete client IP in the socket list.
 
-    log_message(MSG_INFO,"Unicast : Valeur dans le tableau channels de la socket %d\n", &channels[requested_channel-1]);
-    //unicast_del_client(unicast_vars, client, &channels[requested_channel-1]);
+    log_message(MSG_DEBUG,"Unicast : We delete channel n°%d from the socket list\n", requested_channel-1);
+    unicast_del_client(unicast_vars, client, &channels[requested_channel-1]); */
   }
 
   if (0 != unicast_reply_free(reply))
   {
-    log_message(MSG_INFO,"Unicast : Error when releasing the HTTP reply after sendinf it\n");
+    log_message(MSG_INFO,"Unicast : Error when releasing the RTSP reply after sendinf it\n");
     return -1;
   }
 
