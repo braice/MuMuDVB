braice/MuMuDVB

commit 82b962af8feab5a85ebef5f1142ecd49461a69b9

Make this mess compile under windows (mingw for now)

configure.ac
@@ -12,14 +12,14 @@ dnl Check for linux dvb api headers
 dnl
 AC_CHECK_HEADER([linux/dvb/dmx.h])
 if test "$ac_cv_header_linux_dvb_dmx_h" = no; then
-        AC_MSG_ERROR([linux dvb api headers are required to build])
+        AC_MSG_WARN([linux dvb api headers not found, disabling dvb api functionality])
+        AC_DEFINE(DISABLE_DVB_API, 1, Define if you want to bulid without DVB API)
 fi
 
 # Checks for programs.
 AC_PROG_CC
 AC_PROG_INSTALL
 AC_PROG_MAKE_SET
-AC_PROG_CC_C99
 
 dnl
 dnl Iconv stuff

src/unicast_monit.c
@@ -395,6 +395,7 @@ unicast_send_json_state (int number_of_channels, mumudvb_channel_t *channels, in
 
 	// Frontend type
 	char fetype[10]="Unknown";
+#ifndef DISABLE_DVB_API
 	if (strengthparams->tune_p->fe_type==FE_OFDM)
 #ifdef DVBT2
 		if (strengthparams->tune_p->delivery_system==SYS_DVBT2)
@@ -421,9 +422,13 @@ unicast_send_json_state (int number_of_channels, mumudvb_channel_t *channels, in
 		snprintf(fetype,10,"DVB-S");
 #endif
 	}
+#else
+	snprintf(fetype, 10, "File");
+#endif
 	unicast_reply_write(reply, "\t\"frontend_system\" : \"%s\",\n",fetype);
 
 	// Frontend status
+#ifndef DISABLE_DVB_API
 	char SCVYL[6]="-----";
 	if (strengthparams->festatus & FE_HAS_SIGNAL)  SCVYL[0]=83; // S
 	if (strengthparams->festatus & FE_HAS_CARRIER) SCVYL[1]=67; // C
@@ -431,6 +436,9 @@ unicast_send_json_state (int number_of_channels, mumudvb_channel_t *channels, in
 	if (strengthparams->festatus & FE_HAS_SYNC)    SCVYL[3]=89; // Y
 	if (strengthparams->festatus & FE_HAS_LOCK)    SCVYL[4]=76; // L
 	SCVYL[5]=0;
+#else
+	char SCVYL[6] = "SCVYL";
+#endif
 	unicast_reply_write(reply, "\t\"frontend_status\": \"%s\",\n",SCVYL);
 
 	// Frontend signal
@@ -750,6 +758,7 @@ unicast_send_xml_state (int number_of_channels, mumudvb_channel_t *channels, int
 
 	// Frontend type
 	char fetype[10]="Unknown";
+#ifndef DISABLE_DVB_API
 	if (strengthparams->tune_p->fe_type==FE_OFDM)
 #ifdef DVBT2
 		if (strengthparams->tune_p->delivery_system==SYS_DVBT2)
@@ -776,9 +785,13 @@ unicast_send_xml_state (int number_of_channels, mumudvb_channel_t *channels, int
 		snprintf(fetype,10,"DVB-S");
 #endif
 	}
+#else
+	snprintf(fetype, 10, "File");
+#endif
 	unicast_reply_write(reply, "\t<frontend_system><![CDATA[%s]]></frontend_system>\n",fetype);
 
 	// Frontend status
+#ifndef DISABLE_DVB_API
 	char SCVYL[6]="-----";
 	if (strengthparams->festatus & FE_HAS_SIGNAL)  SCVYL[0]=83; // S
 	if (strengthparams->festatus & FE_HAS_CARRIER) SCVYL[1]=67; // C
@@ -786,6 +799,9 @@ unicast_send_xml_state (int number_of_channels, mumudvb_channel_t *channels, int
 	if (strengthparams->festatus & FE_HAS_SYNC)    SCVYL[3]=89; // Y
 	if (strengthparams->festatus & FE_HAS_LOCK)    SCVYL[4]=76; // L
 	SCVYL[5]=0;
+#else
+	char SCVYL[6] = "SCVYL";
+#endif
 	unicast_reply_write(reply, "\t<frontend_status><![CDATA[%s]]></frontend_status>\n",SCVYL);
 
 	// Frontend signal

.gitignore
@@ -13,3 +13,5 @@ config.*
 /src/stamp-h1
 *.o
 *.sw[po]
+.vs/
+MuMuDVB.vcxproj.user

src/autoconf.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * MuMuDVB - Stream a DVB transport stream.
  * File for Autoconfiguration
  *
@@ -29,11 +29,11 @@
 
 /** @file
  *  @brief This file contain the code related to the autoconfiguration of MuMudvb
- * 
+ *
  *  It contains the functions to extract the relevant informations from the PAT,PMT,SDT PIDs and from ATSC PSIP table
- * 
+ *
  *  The PAT contains the list of the channels in the actual stream, their service number and the PMT PID
- * 
+ *
  *  The SDT contains the name of the channels associated to a certain service number and the type of service
  *
  *  The PSIP (ATSC only) table contains the same kind of information as the SDT
@@ -53,8 +53,11 @@
  *  This allows to keep the IP if the channel comes back
  */
 
-
+#ifndef _WIN32
 #include <sys/ioctl.h>
+#include <sys/poll.h>
+#endif
+
 #include <errno.h>
 #include <stdint.h>
 
@@ -66,7 +69,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <sys/poll.h>
 
 
 #include "errors.h"

src/dvb.c
@@ -1,23 +1,23 @@
 /* dvb.c
  * MuMuDVB - Stream a DVB transport stream.
- * 
+ *
  * (C) 2004-2013 Brice DUBOST
  * (C) Dave Chapman <dave@dchapman.com> 2001, 2002.
- * 
+ *
  * The latest version can be found at http://mumudvb.net
- * 
+ *
  * Copyright notice:
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -43,22 +43,26 @@
 
 static char *log_module="DVB: ";
 
+#ifndef O_NONBLOCK
+#define O_NONBLOCK 0
+#endif
+
 /**
  * @brief Check the file exists and is readable
  * Return 1 in case it exists, 0 otherwise
  */
 static int file_exists(char *filename)
 {
-	struct stat   buffer;   
+	struct stat   buffer;
 	return (stat(filename, &buffer) == 0) ? 1 : 0;
 }
 
 /**
  * @brief Open the frontend associated with card
  * Return 1 in case of succes, -1 otherwise
- * 
- * @param fd_frontend the file descriptor for the frontend 
- * @param card the card number 
+ *
+ * @param fd_frontend the file descriptor for the frontend
+ * @param card the card number
  */
 int
 open_fe (int *fd_frontend, char *base_path, int tuner, int rw, int full_path)
@@ -97,6 +101,7 @@ open_fe (int *fd_frontend, char *base_path, int tuner, int rw, int full_path)
 void
 set_ts_filt (int fd, uint16_t pid)
 {
+#ifndef DISABLE_DVB_API
 	struct dmx_pes_filter_params pesFilterParams;
 
 	log_message( log_module,  MSG_DEBUG, "Setting filter for PID %d\n", pid);
@@ -113,6 +118,10 @@ set_ts_filt (int fd, uint16_t pid)
 		log_message( log_module,  MSG_ERROR, "FILTER %i: ", pid);
 		log_message( log_module,  MSG_ERROR, "DMX SET PES FILTER : %s\n", strerror(errno));
 	}
+#else
+	(void)fd;
+	(void)pid;
+#endif
 }
 
 /**
@@ -122,6 +131,7 @@ set_ts_filt (int fd, uint16_t pid)
  */
 void *show_power_func(void* arg)
 {
+#ifndef DISABLE_DVB_API
 	strength_parameters_t  *strengthparams;
 	strengthparams= (strength_parameters_t  *) arg;
 	fe_status_t festatus_old;
@@ -220,12 +230,15 @@ void *show_power_func(void* arg)
 		for(i=0;i<wait_time && !strengthparams->tune_p->strengththreadshutdown;i++)
 			usleep(100000);
 	}
+#else
+	(void)arg;
+#endif
 	return 0;
 }
 
 
 /**
- * @brief Open file descriptors for the card. open dvr and one demuxer fd per asked pid. This function can be called 
+ * @brief Open file descriptors for the card. open dvr and one demuxer fd per asked pid. This function can be called
  * more than one time if new pids are added (typical case autoconf)
  * return -1 in case of error
  * @param card the card number
@@ -255,15 +268,18 @@ create_card_fd(char *base_path, int tuner, uint8_t *asked_pid, fds_t *fds)
 	for(curr_pid=0;curr_pid<8193;curr_pid++)
 		//file descriptors for the demuxer (used to set the filters)
 		//we check if we need to open the file descriptor (some cards are limited)
-		if ((asked_pid[curr_pid] != 0)&& (fds->fd_demuxer[curr_pid]==0) )
-			if((fds->fd_demuxer[curr_pid] = open (demuxdev_name, O_RDWR)) < 0)
-			{
-				log_message( log_module,  MSG_ERROR, "FD PID %i: ", curr_pid);
-				log_message( log_module,  MSG_ERROR, "DEMUX DEVICE: %s : %s\n", demuxdev_name, strerror(errno));
+		if ((asked_pid[curr_pid] != 0) && (fds->fd_demuxer[curr_pid] == 0)) {
+#ifndef DISABLE_DVB_API
+			if ((fds->fd_demuxer[curr_pid] = open(demuxdev_name, O_RDWR)) < 0) {
+				log_message(log_module, MSG_ERROR, "FD PID %i: ", curr_pid);
+				log_message(log_module, MSG_ERROR, "DEMUX DEVICE: %s : %s\n", demuxdev_name, strerror(errno));
 				free(demuxdev_name);
 				return -1;
 			}
-
+#else
+			fds->fd_demuxer[curr_pid] = -1;
+#endif
+		}
 
 	asprintf_ret=asprintf(&dvrdev_name,"%s/%s%d",base_path,DVR_DEV_NAME,tuner);
 	if(asprintf_ret==-1)
@@ -293,7 +309,7 @@ create_card_fd(char *base_path, int tuner, uint8_t *asked_pid, fds_t *fds)
 
 
 /**
- * @brief Open filters for the pids in asked_pid. This function update the asked_pid array and 
+ * @brief Open filters for the pids in asked_pid. This function update the asked_pid array and
  * can be called more than one time if new pids are added (typical case autoconf)
  * Ie it asks the card for the pid list by calling set_ts_filt
  * @param asked_pid the array of asked pids
@@ -410,7 +426,7 @@ void *read_card_thread_func(void* arg)
 
 /** @brief : Read data from the card
  * This function have to be called after a poll to ensure there is data to read
- * 
+ *
  */
 int card_read(int fd_dvr, unsigned char *dest_buffer, card_buffer_t *card_buffer)
 {
@@ -448,11 +464,12 @@ typedef struct frontend_cap_t
 }frontend_cap_t;
 
 /** @brief : List the capabilities of one card
- * 
- * 
+ *
+ *
  */
 void show_card_capabilities( int card, int tuner )
 {
+#ifndef DISABLE_DVB_API
 	int frontend_fd;
 	int i_ret;
 	int display_sr;
@@ -548,11 +565,14 @@ void show_card_capabilities( int card, int tuner )
 	close (frontend_fd);
 
 	log_message( log_module,  MSG_INFO, "\n");
-
+#else
+	(void)card;
+	(void)tuner;
+#endif
 }
 
 /** @brief : List the DVB cards of the system and their capabilities
- * 
+ *
  */
 void list_dvb_cards ()
 {

src/dvb.h
@@ -1,23 +1,23 @@
-/* 
+/*
  * mumudvb - UDP-ize a DVB transport stream.
  * Based on dvbstream by (C) Dave Chapman <dave@dchapman.com> 2001, 2002.
- * 
+ *
  * (C) 2004-2010 Brice DUBOST
- * 
+ *
  * The latest version can be found at http://mumudvb.net/
- * 
+ *
  * Copyright notice:
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -33,18 +33,23 @@
 #ifndef _DVB_H
 #define _DVB_H
 
-#include <syslog.h>
 #include <stdio.h>
+#ifndef _WIN32
+#include <syslog.h>
 #include <sys/ioctl.h>
+#include <sys/poll.h>
+#include <resolv.h>
+#endif
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <resolv.h>
-#include <sys/poll.h>
+#include "config.h"
 
-// DVB includes:
+#ifndef DISABLE_DVB_API
+ // DVB includes:
 #include <linux/dvb/dmx.h>
 #include <linux/dvb/frontend.h>
+#endif
 
 #include "mumudvb.h"
 #include "tune.h"
@@ -105,5 +110,5 @@ void close_card_fd(fds_t *fds);
 void *show_power_func(void* arg);
 int card_read(int fd_dvr, unsigned char *dest_buffer, card_buffer_t *card_buffer);
 
-void list_dvb_cards ();
+void list_dvb_cards(void);
 #endif

src/log.c
@@ -1,23 +1,23 @@
-/* 
+/*
  * mumudvb - Stream a DVB transport stream.
  * Based on dvbstream by (C) Dave Chapman <dave@dchapman.com> 2001, 2002.
- * 
+ *
  * (C) 2004-2013 Brice DUBOST
- * 
+ *
  * The latest version can be found at http://mumudvb.net
- * 
+ *
  * Copyright notice:
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -26,22 +26,30 @@
 
 /** @file
  * @brief Log functions for mumudvb
- * 
+ *
  * This file contains functions to log messages or write logging information to a file
  */
+
+#define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
 #include <stddef.h>
+#ifndef _WIN32
 #include <syslog.h>
+#endif
 #include <errno.h>
-#include <linux/dvb/version.h>
 #include <time.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <inttypes.h>
 
+#include "config.h"
+
+#ifndef DISABLE_DVB_API
+#include <linux/dvb/version.h>
+#endif
 
 #include "mumudvb.h"
 #include "errors.h"
@@ -140,12 +148,14 @@ int read_logging_configuration(stats_infos_t *stats_infos, char *substring)
 		substring = strtok (NULL, delimiteurs);
 		if (!strcmp (substring, "console"))
 			log_params.log_type |= LOGGING_CONSOLE;
+#ifndef _WIN32
 		else if (!strcmp (substring, "syslog"))
 		{
 			openlog ("MUMUDVB", LOG_PID, 0);
 			log_params.log_type |= LOGGING_SYSLOG;
 			log_params.syslog_initialised=1;
 		}
+#endif
 		else
 			log_message(log_module,MSG_WARN,"Invalid value for log_type\n");
 	}
@@ -280,8 +290,10 @@ void log_message( char* log_module, int type,
 		{
 			if (log_params.log_type == LOGGING_FILE)
 				fprintf( log_params.log_file,"Problem with malloc : %s file : %s line %d\n",strerror(errno),__FILE__,__LINE__);
+#ifndef _WIN32
 			else if (log_params.log_type == LOGGING_SYSLOG)
 				syslog (MSG_ERROR,"Problem with malloc : %s file : %s line %d\n",strerror(errno),__FILE__,__LINE__);
+#endif
 			else
 				fprintf( stderr,"Problem with malloc : %s file : %s line %d\n",strerror(errno),__FILE__,__LINE__);
 			va_end( args );
@@ -329,8 +341,10 @@ void log_message( char* log_module, int type,
 	{
 		if (log_params.log_type == LOGGING_FILE)
 			fprintf( log_params.log_file,"Problem with malloc : %s file : %s line %d\n",strerror(errno),__FILE__,__LINE__);
+#ifndef _WIN32
 		else if (log_params.log_type == LOGGING_SYSLOG)
 			syslog (MSG_ERROR,"Problem with malloc : %s file : %s line %d\n",strerror(errno),__FILE__,__LINE__);
+#endif
 		else
 			fprintf( stderr,"Problem with malloc : %s file : %s line %d\n",strerror(errno),__FILE__,__LINE__);
 		va_end( args );
@@ -355,6 +369,7 @@ void log_message( char* log_module, int type,
 	{
 		if ( log_params.log_type & LOGGING_FILE)
 			fprintf(log_params.log_file,"%s",log_string.string);
+#ifndef _WIN32
 		if((log_params.log_type & LOGGING_SYSLOG) && (log_params.syslog_initialised))
 		{
 			//what is the priority ?
@@ -382,13 +397,13 @@ void log_message( char* log_module, int type,
 			}
 			syslog (priority,"%s",log_string.string);
 		}
+#endif
 		if((log_params.log_type == LOGGING_UNDEFINED) ||
 				(log_params.log_type & LOGGING_CONSOLE) ||
 				((log_params.log_type & LOGGING_SYSLOG) && (log_params.syslog_initialised==0)))
 			fprintf(stderr,"%s",log_string.string);
 	}
 	mumu_free_string(&log_string);
-
 }
 
 /**
@@ -673,7 +688,7 @@ ca_sys_id_t casysids[]={
 int num_casysids=130;
 
 
-/** @brief Display the ca system id according to ETR 162 
+/** @brief Display the ca system id according to ETR 162
  *
  * @param id the id to display
  */
@@ -965,7 +980,7 @@ void print_info ()
 			"by Brice DUBOST (mumudvb@braice.net)\n\n"
 #if DVB_API_VERSION >= 5
 			,DVB_API_VERSION,DVB_API_VERSION_MINOR
-#endif			
+#endif
 			);
 
 }

src/multicast.c
@@ -13,24 +13,26 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  */
- 
+
 
 
 #include "mumudvb.h"
 #include "log.h"
 #include <string.h>
+#ifndef _WIN32
 #include <net/if.h>
+#endif
 static char *log_module="Multicast: ";
 
 
@@ -171,5 +173,5 @@ int read_multicast_configuration(multi_p_t *multi_p, mumudvb_channel_t *c_chan, 
     return 0; //Nothing concerning multicast, we return 0 to explore the other possibilities
 
   return 1;//We found something for multicast, we tell main to go for the next line
-  
+
 }

commit d7032a4cc3e247c127010ce54c8fe4e98134f8e6

Fix broken linux build: loop initial declarations are only allowed in C99 or C11 mode (broken by 82b962af8feab5a85ebef5f1142ecd49461a69b9) error: conflicting types for scam_vars

configure.ac
@@ -18,6 +18,7 @@ fi
 
 # Checks for programs.
 AC_PROG_CC
+AC_PROG_CC_STDC
 AC_PROG_INSTALL
 AC_PROG_MAKE_SET
 

src/unicast_monit.c
@@ -639,7 +639,6 @@ int unicast_send_channel_list_xml (int number_of_channels, mumudvb_channel_t *ch
 #ifndef ENABLE_SCAM_SUPPORT
     (void) scam_vars_v; //to make compiler happy
 #else
-    char *scam_vars;
     scam_parameters_t *scam_vars=(scam_parameters_t *)scam_vars_v;
 #endif
 